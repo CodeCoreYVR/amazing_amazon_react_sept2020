b046392046ac7f8ca1add9f45e9a5b439936570d
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-12-16 17:31:17 -0800

    delete product from index

--- src/components/ProductIndexPage.js
+++ src/components/ProductIndexPage.js
@@ -7,8 +7,16 @@ class ProductIndexPage extends Component {
     this.state = {
       products: [...productList],
     };
+
+    this.deleteProduct = this.deleteProduct.bind(this);
   }
 
+  deleteProduct(productId) {
+    this.setState({
+      products: this.state.products.filter(q => q.id !== productId),
+    });
+  }
+  
   render() {
     return (
       <div className="ProductIndexPage">
@@ -20,6 +28,9 @@ class ProductIndexPage extends Component {
                 <em>{product.id}</em>
               </small>{' '}
               <a href="#">{product.title}</a>
+              <button style={{ borderColor: 'red', margin: '5px 6px' }} onClick={() => this.deleteProduct(product.id)}>
+                Delete
+              </button>
             </li>
           ))}
         </ul>
